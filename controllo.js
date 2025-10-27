75924a23719fa6081201a08704e4e092694c2e73
AleStore-Official <228321164+AleStore-Official@users.noreply.github.com>
2025-10-27 21:12:49 +0100

    Update controllo.js

--- controllo.js
+++ controllo.js
@@ -1,9 +1,9 @@
 (function () {
   const accesso = localStorage.getItem("accesso_verificato");
 
+  // Se non è stato verificato, reindirizza a reCAPTCHA con ritorno garantito
   if (!accesso || accesso !== "true") {
     const currentUrl = encodeURIComponent(window.location.href);
-    const recaptchaUrl = `https://alestore-official.github.io/AleCAPTCHA/recaptcha.html?redirect=${currentUrl}`;
-    window.location.href = recaptchaUrl;
+    window.location.replace(`https://alestore-official.github.io/AleCAPTCHA/recaptcha.html?redirect=${currentUrl}`);
   }
 })();
