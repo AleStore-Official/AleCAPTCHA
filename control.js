04494d53e4a30f4dc7498c3f3487502897aeadce
AleStore-Official <228321164+AleStore-Official@users.noreply.github.com>
2025-10-28 11:53:55 +0100

    Update control.js

--- control.js
+++ control.js
@@ -1,7 +1,11 @@
 (function () {
-  const accesso = localStorage.getItem("accesso_verificato");
+  const verified = localStorage.getItem("access_verified");
 
-  if (!accesso || accesso !== "true") {
+  if (!verified) {
+    // Save the current page as origin
+    localStorage.setItem("origin_page", window.location.href);
+
+    // Redirect to reCAPTCHA verification
     window.location.href = "https://alestore-official.github.io/AleCAPTCHA";
   }
 })();
