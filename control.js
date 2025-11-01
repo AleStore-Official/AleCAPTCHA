545600400501ca51ab50e76530f9115e57fafdad
AleStore-Official <228321164+AleStore-Official@users.noreply.github.com>
2025-11-01 23:47:38 +0100

    Update control.js

--- control.js
+++ control.js
@@ -1,6 +1,7 @@
 <script type="module">
   import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
+  // Configurazione Supabase
   const supabase = createClient(
     "https://hsyyrcbibohwvbuwxwok.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzeXlyY2JpYm9od3ZidXd4d29rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwNjE0MDcsImV4cCI6MjA3NjYzNzQwN30.2KsFsGYjwf_cA7Z9oglVthiaE1_jWYuQ6HMMm5UXsyo"
@@ -10,12 +11,14 @@
     const verified = localStorage.getItem("access_verified");
     const registered = localStorage.getItem("user_verified");
 
+    // Controllo localStorage
     if (verified !== "true" || registered !== "true") {
       localStorage.setItem("origin_page", window.location.href);
       window.location.href = "https://alestore-official.github.io/AleCAPTCHA";
       return;
     }
 
+    // Controllo autenticazione Supabase
     const { data: { user }, error: authError } = await supabase.auth.getUser();
     if (!user || authError) {
       localStorage.setItem("origin_page", window.location.href);
@@ -23,6 +26,7 @@
       return;
     }
 
+    // Controllo presenza nella tabella 'utenti'
     const { data, error } = await supabase
       .from("utenti")
       .select("email")
