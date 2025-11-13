caf9e55714309e5ed0ac4e25d9d4aec165458978
AleStore-Official <228321164+AleStore-Official@users.noreply.github.com>
2025-11-13 17:41:34 +0100

    Update control.js

--- control.js
+++ control.js
@@ -3,6 +3,6 @@
 
   if (verified !== "true") {
     localStorage.setItem("origin_page", window.location.href);
-    window.location.href = "https://alestore-official.github.io/AleCAPTCHA";
+    window.location.replace("https://alestore-official.github.io/AleCAPTCHA");
   }
 })();
