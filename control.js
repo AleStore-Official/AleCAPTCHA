bd766990c2f073f693db7b1bf5c3edace4e73eb1
AleStore-Official <228321164+AleStore-Official@users.noreply.github.com>
2025-11-27 14:54:49 +0100

    Update control.js

--- control.js
+++ control.js
@@ -1,8 +1,12 @@
 (function () {
-  const verified = localStorage.getItem("captcha_passed");
-
-  if (verified !== "true") {
-    localStorage.setItem("origin_page", window.location.href);
-    window.location.replace("https://alestore-official.github.io/AleCAPTCHA");
+  function checkAccess() {
+    const verified = localStorage.getItem("captcha_passed");
+    if (verified !== "true") {
+      localStorage.setItem("origin_page", window.location.href);
+      window.location.replace("https://alestore-official.github.io/AleCAPTCHA");
+    }
   }
+
+  checkAccess();
+  setInterval(checkAccess, 6000);
 })();
